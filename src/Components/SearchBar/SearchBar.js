17af406b79d855b5cac68314ba7b6ce1098cab3e
Sandip Sen <umel@protonmail.com>
2017-12-12 09:18:27 +0530

    Stuck with step 79

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,10 +2,26 @@ import React, { Component } from 'react';
 import './SearchBar.css';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {searchTerm: ""};
+    
+    this.search = this.search.bind(this);
+    this.handleTermChange = this.handleTermChange.bind(this);
+  }
+
+  search() {
+    this.props.onSearch(this.state.searchTerm);
+  }
+
+  handleTermChange(e) {
+    this.setSate({searchTerm: e.target.value})
+  }
+
   render () {
     return (
       <div className="SearchBar">
-        <input placeholder="Enter A Song, Album, or Artist" />
+        <input onchange={this.handleTermChange}placeholder="Enter A Song, Album, or Artist" />
         <a>SEARCH</a>
       </div>
     );
