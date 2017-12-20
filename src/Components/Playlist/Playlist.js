a374d955ddb2f82df13066dc3e997fe7de889be1
Sandip Sen <umel@protonmail.com>
2017-12-20 21:40:07 +0530

    Completes Jamming

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -14,7 +14,7 @@ class Playlist extends Component {
   render() {
     return (
       <div className="Playlist">
-        <input defaultValue={this.props.playListName} onChange={this.handleNameChange}/>
+        <input value={this.props.playListName} onChange={this.handleNameChange}/>
         <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={true} />
         <a className="Playlist-save" onClick={this.props.onSave}>
         SAVE TO SPOTIFY
